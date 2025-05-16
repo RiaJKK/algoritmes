877dc8c31c5d25d6bea01efeaf560b816b60fd6e
RiaJ <koselevaviktoria12@gmail.com>
2025-05-16 17:03:52 +0700

    Bubble sort finished

--- 4_bubble_sort.js
+++ 4_bubble_sort.js
@@ -1,13 +1,19 @@
 // import arrForSort;
-import arrForSort from "./arrays";
-
+import arrForSort from "./arrays.js";
 
 function bubbleSort(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] < arr[i+1]) {
-            [arr[i], arr[i + 1]] = [arr[i+1], arr[i]];
-        }
+    // console.log(arr[i] , arr[i+1]);
+    let end = arr.length - 1;
+
+    for (let j = 0; j < arr.length; j++) {
+        for (let i = 0; i < end; i++) {
+            if (arr[i] > arr[i+1]) {
+                [arr[i], arr[i + 1]] = [arr[i+1], arr[i]];
+
+            }
+        } end--;
     }
+    return arr;
 }
 
-console.log(arrForSort)
+console.log(bubbleSort(arrForSort));
